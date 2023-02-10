eba528340db284240d7053ab505cd210b566e70e
Eduardo <eduardo.r.trabalho@gmail.com>
2023-02-10 01:00:28 -0300

    Exemplo 2 do while.

---
+++ Exercicios/src/br/com/exercicios/loops/Ex2_Nota.java
@@ -0,0 +1,23 @@
+package br.com.exercicios.loops;
+
+import java.util.Scanner;
+
+public class Ex2_Nota {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota > 10) {
+            System.out.println("Nota ivalida! Digite novamente: ");
+            nota = scan.nextInt();
+            
+        }
+    }
+
+}
